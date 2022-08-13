9b848917e92de943bf87280ac678e2c29695dce4
djuturum <hladila.potkunica@gmail.com>
2022-08-13 07:11:26 -0700

    w

--- docker-demo-3/backend.tf
+++ docker-demo-3/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-a33b6219"
+    bucket = "terraform-state-a44b6219"
     key    = "terraform.tfstate"
     region = "eu-central-1"
   }
--- docker-demo-3/s3.tf
+++ docker-demo-3/s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform-state" {
-  bucket = "terraform-state-a33b6219"
+  bucket = "terraform-state-a44b6219"
 
   tags = {
     Name = "Terraform state"
