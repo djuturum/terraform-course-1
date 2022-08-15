4705630afe0c4d164a723f796b71aa5095267f47
djuturum <48289390+djuturum@users.noreply.github.com>
2022-08-15 14:43:03 +0200

    x

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -11,11 +11,9 @@ variable "AWS_REGION" {
 variable "AMIS" {
   type = map(string)
   default = {
-
     us-east-1 = "ami-13be557e muamer 23"
     us-west-2 = "ami-06b94666"
     eu-central-1 = "ami-0c956e207f9d113d5"
-
   }
 }
 
