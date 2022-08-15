bc0267fbc2f3a1db75cdf2ec5eec2b730f975765
djuturum <48289390+djuturum@users.noreply.github.com>
2022-08-15 16:31:14 +0200

    d

--- docker-demo-3/backend.tf
+++ docker-demo-3/backend.tf
@@ -1,7 +1,7 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "terraform-state-b44b6219"
-#     key    = "terraform.tfstate"
-#     region = "eu-central-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-b44b6219"
+    key    = "terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
